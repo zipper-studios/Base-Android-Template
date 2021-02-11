382631a4d26a41d5de30c8499d26e00ab8f8c6d7
Loredana Petrea <loredana.petrea@zipperstudios.co>
2021-02-11 13:32:14 +0200

    Added the GithubUsersFragment as the start destination for main activity. Fixed the issue where GithubUsersRepository cannot be injected

--- app/src/main/java/com/base_android_template/di/RepositoryModule.kt
+++ app/src/main/java/com/base_android_template/di/RepositoryModule.kt
@@ -1,8 +1,9 @@
 package com.base_android_template.di
 
+import com.base_android_template.repository.GithubUsersRepository
 import com.base_android_template.repository.GithubUsersRepositoryImpl
 import org.koin.dsl.module
 
 val repositoryModule = module {
-    factory { GithubUsersRepositoryImpl(githubUsersApi = get()) }
+    factory<GithubUsersRepository> { GithubUsersRepositoryImpl(githubUsersApi = get()) }
 }
