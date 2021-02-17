20fd62c057fb25b9c4912f4e330620d988086de5
Loredana Petrea <loredana.petrea@zipperstudios.co>
2021-02-17 16:48:22 +0200

    Added the dependency for Glide library. Implemented a common method to load image into ImageView using binding adapter. Display the avatar for each Github user

--- app/src/main/java/com/base_android_template/utils/BindingAdapters.kt
+++ app/src/main/java/com/base_android_template/utils/BindingAdapters.kt
@@ -1,7 +1,13 @@
 package com.base_android_template.utils
 
+import android.widget.ImageView
+import androidx.appcompat.content.res.AppCompatResources.getDrawable
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.base_android_template.R
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
 
 
 object BindingAdapters {
@@ -13,5 +19,22 @@ object BindingAdapters {
         adapter: RecyclerView.Adapter<RecyclerView.ViewHolder>
     ) {
         recyclerView.adapter = adapter
+    }
+
+    @BindingAdapter("app:loadImage")
+    @JvmStatic
+    fun bindLoadImageMethod(imageView: ImageView, data: String?) {
+        val options = RequestOptions()
+            .centerCrop()
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .dontAnimate()
+
+        Glide.with(imageView.context)
+            .asBitmap()
+            .placeholder(getDrawable(imageView.context, R.drawable.bg_gray_rounded))
+            .error(getDrawable(imageView.context, R.drawable.bg_gray_rounded))
+            .load(data ?: "")
+            .apply(options)
+            .into(imageView)
     }
 }
