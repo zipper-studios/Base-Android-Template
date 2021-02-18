1c23101f46970affd531b4acd1735e8a938abaf9
Loredana Petrea <loredana.petrea@zipperstudios.co>
2021-02-18 12:38:10 +0200

    Added Calligraphy dependency and made the setup for calligraphy

--- app/src/main/java/com/base_android_template/App.kt
+++ app/src/main/java/com/base_android_template/App.kt
@@ -3,6 +3,10 @@ package com.base_android_template
 import android.app.Application
 import com.base_android_template.di.createCoreModules
 import com.base_android_template.feature.github_users.githubUsersModule
+import com.orhanobut.hawk.Hawk
+import io.github.inflationx.calligraphy3.CalligraphyConfig
+import io.github.inflationx.calligraphy3.CalligraphyInterceptor
+import io.github.inflationx.viewpump.ViewPump
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
@@ -14,8 +18,26 @@ class App : Application() {
             androidContext(this@App)
             modules(getAppModules())
         }
+
+        Hawk.init(this).build()
+
+        initCalligraphy()
     }
 
     private fun getAppModules() = createCoreModules() +
             githubUsersModule
+
+    private fun initCalligraphy() {
+        ViewPump.init(
+            ViewPump.builder()
+                .addInterceptor(
+                    CalligraphyInterceptor(
+                        CalligraphyConfig.Builder()
+                            .setDefaultFontPath("fonts/lato_regular.ttf")
+                            .build()
+                    )
+                )
+                .build()
+        )
+    }
 }
