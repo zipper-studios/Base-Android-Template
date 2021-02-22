683d591b3f6e704711dce2033a8c8b6aae233916
Loredana Petrea <loredana.petrea@zipperstudios.co>
2021-02-22 15:36:19 +0200

    Commented the BindingAdapters class and its methods

--- app/src/main/java/com/base_android_template/utils/binding_adapter/BindingAdapters.kt
+++ app/src/main/java/com/base_android_template/utils/binding_adapter/BindingAdapters.kt
@@ -9,9 +9,17 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.bumptech.glide.request.RequestOptions
 
-
+/*
+ * Binding adapters are responsible for making the appropriate framework calls to set values for views directly inside layout.
+ */
 object BindingAdapters {
 
+    /**
+     * Set the adapter on RecyclerView
+     *
+     * @param recyclerView RecyclerView. The RecyclerView on which the adapter will be set
+     * @param adapter RecyclerView.Adapter<RecyclerView.ViewHolder>. The adapter instance from ViewModel that will be set on RecyclerView
+     */
     @JvmStatic
     @BindingAdapter("adapter")
     fun setAdapter(
@@ -21,9 +29,15 @@ object BindingAdapters {
         recyclerView.adapter = adapter
     }
 
+    /**
+     * Load image url ito ImageView using Glide
+     *
+     * @param imageView ImageView. The ImageView that will display the image
+     * @param imageUrl String?. The url of the image that will be displayed
+     */
     @BindingAdapter("app:loadImage")
     @JvmStatic
-    fun bindLoadImageMethod(imageView: ImageView, data: String?) {
+    fun bindLoadImageMethod(imageView: ImageView, imageUrl: String?) {
         val options = RequestOptions()
             .centerCrop()
             .diskCacheStrategy(DiskCacheStrategy.ALL)
@@ -33,7 +47,7 @@ object BindingAdapters {
             .asBitmap()
             .placeholder(getDrawable(imageView.context, R.drawable.bg_gray_rounded))
             .error(getDrawable(imageView.context, R.drawable.bg_gray_rounded))
-            .load(data ?: "")
+            .load(imageUrl ?: "")
             .apply(options)
             .into(imageView)
     }
