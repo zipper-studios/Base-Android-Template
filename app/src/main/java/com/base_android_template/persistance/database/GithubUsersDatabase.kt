5818ff37436e0af3cacc3af46efc94515ebde836
Loredana Petrea <loredana.petrea@zipperstudios.co>
2021-02-26 11:38:23 +0200

    Added entities to GihubUsersDatabase

--- app/src/main/java/com/base_android_template/persistance/database/GithubUsersDatabase.kt
+++ app/src/main/java/com/base_android_template/persistance/database/GithubUsersDatabase.kt
@@ -1,4 +1,12 @@
 package com.base_android_template.persistance.database
 
-class GithubUsersDatabase {
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.base_android_template.model.response.GithubUserResponse
+import com.base_android_template.persistance.dao.GithubUsersDao
+
+@Database(entities = [GithubUserResponse::class], version = 1)
+abstract class GithubUsersDatabase : RoomDatabase() {
+
+    abstract fun githubUsersDao(): GithubUsersDao
 }
