99f6bc62cdebbc29d82eb2c48187aa847df708b3
Loredana Petrea <loredana.petrea@zipperstudios.co>
2021-03-17 13:26:49 +0200

    Commented BaseViewModel class and methods

--- app/src/main/java/com/base_android_template/base/BaseViewModel.kt
+++ app/src/main/java/com/base_android_template/base/BaseViewModel.kt
@@ -6,6 +6,12 @@ import androidx.lifecycle.ViewModel
 import androidx.navigation.NavDirections
 import com.base_android_template.shared.model.NavigationCommand
 
+/**
+ * Represents the base class that will be extended by any ViewModel in the app.
+ * It contains public methods that can be called from all ViewModels in the hierarchy
+ * to send commands to the BaseFragment
+ *
+ */
 open class BaseViewModel : ViewModel() {
 
     val navigationCommand: LiveData<NavigationCommand?>
@@ -19,10 +25,20 @@ open class BaseViewModel : ViewModel() {
     private val _loading = MutableLiveData<Boolean>()
     private val _message = MutableLiveData<String>()
 
+    /**
+     * Call this method when want to navigate between fragments via NavDirections
+     *
+     * @param direction NavDirections. The generated method for navigation action
+     * that you've defined in the nav graph
+     */
     fun postNavigationCommand(direction: NavDirections) {
         _navigationCommand.postValue(NavigationCommand.PerformNavAction(direction))
     }
 
+    /**
+     * Call this method when want to navigate up in fragments
+     *
+     */
     fun postNavigateUpCommand() {
         _navigationCommand.postValue(NavigationCommand.PerformNavUp)
     }
@@ -31,10 +47,11 @@ open class BaseViewModel : ViewModel() {
         _navigationCommand.value = null
     }
 
-    fun postLoading(loading: Boolean) {
-        _loading.postValue(loading)
-    }
-
+    /**
+     * Call this method when want to display a message in a Toast
+     *
+     * @param toDisplay String. The message to be displayed
+     */
     fun postMessage(toDisplay: String) {
         _message.postValue(toDisplay)
     }
@@ -42,4 +59,16 @@ open class BaseViewModel : ViewModel() {
     fun clearLastMessage() {
         _message.value = ""
     }
+
+    /**
+     * Call this method when want to show/hide the loading progress bar
+     *
+     * @param loading Boolean. If:
+     * - true: show the loading progress bar
+     * - false: hide th loading progress bar
+     */
+    fun postLoading(loading: Boolean) {
+        _loading.postValue(loading)
+    }
+
 }
